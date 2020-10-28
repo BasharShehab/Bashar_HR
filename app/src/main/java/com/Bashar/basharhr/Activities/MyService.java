ac576deeda7241ad55e242e06e759ca882faf3be
basharHR <beshrshehab999@gmail.com>
2020-10-28 23:45:24 +0300

    added music player

---
+++ app/src/main/java/com/Bashar/basharhr/Activities/MyService.java
@@ -0,0 +1,44 @@
+package com.Bashar.basharhr.Activities;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.Bashar.basharhr.R;
+
+public class MyService extends Service {
+    public MyService() {
+
+    }
+    MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "Service Created!", Toast.LENGTH_SHORT).show();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.elevatormusic);
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid){
+
+        mediaPlayer.start();
+        Toast.makeText(this, "Music Started", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onDestroy(){
+        mediaPlayer.stop();
+        Toast.makeText(this, "Music Stopped", Toast.LENGTH_SHORT).show();
+    }
+}
